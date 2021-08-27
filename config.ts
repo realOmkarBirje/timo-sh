39d13a37836399658279c71e70b9974b78278a65
realOmkarBirje <89650949+realOmkarBirje@users.noreply.github.com>
2021-08-27 22:46:18 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-const githubToken = process.env.GITHUB_TOKEN;
+const githubToken = process.env.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;
 
 if (!githubToken) {
   throw new Error("Missing environment variable GITHUB_TOKEN.");
